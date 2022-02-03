eb08af21bd583ba3e6daeded01b4d0d889d073bb
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-02-03 10:03:20 -0300

    Add unidentified checkboxes

--- src/components/Unidentified.js
+++ src/components/Unidentified.js
@@ -1,12 +1,23 @@
+import React from "react";
+import { TextField, Button } from "@material-ui/core";
+
 function Unidentified() {
   return (
     <div>
       <TextField
         id="outlined-textarea"
-        label="Descrição adicional do problema"
+        label="Descrição adicional"
         placeholder="Escreva aqui..."
         multiline
       />
+      <p>Caso sinta necessidade, entre em contato com um CCO:</p>
+      <Button
+        variant="contained"
+        href="https://servicos.compesa.com.br"
+        target="_blank"
+      >
+        Entrar em contato com CCO
+      </Button>
     </div>
   );
 }
