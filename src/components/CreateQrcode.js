b16f5353be1ebfe3afc7f75f7b9f0d13b1e91f37
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-02-09 16:53:54 -0300

    Adds text fields for QR Code generator

--- src/components/CreateQrcode.js
+++ src/components/CreateQrcode.js
@@ -10,15 +10,31 @@ import QRCode from "qrcode";
 
 function CreateQrCode() {
   // States do app
-  const [text, setText] = useState("");
+  // const [text, setText] = useState("");
   const [imageUrl, setImageUrl] = useState("");
+
+  const [nomeAtivo, setNomeAtivo] = useState("");
+  const [tombamento, setTombamento] = useState("");
+  const [unidade, setUnidade] = useState("");
+  const [setor, setSetor] = useState("");
 
   const classes = useStyles();
 
   // codigo responsavel por gerar o QR code a partir do dado passado para o input
   const generateQrCode = async () => {
     try {
-      const response = await QRCode.toDataURL(text);
+      // const response = await QRCode.toDataURL(text);
+      const response = await QRCode.toDataURL(
+        '"{\\"tombamento\\":' +
+          parseInt(tombamento) +
+          ',\\"unidade\\":\\"' +
+          unidade +
+          '\\",\\"setor\\":\\"' +
+          setor +
+          '\\",\\"ativo\\":\\"' +
+          nomeAtivo +
+          '\\"}"'
+      );
       setImageUrl(response);
     } catch (error) {
       console.log(error);
@@ -28,10 +44,44 @@ function CreateQrCode() {
   return (
     <Card>
       <CardContent>
+        {/**********************************/}
+
         <TextField
-          label="Insira aqui os dados"
-          onChange={(e) => setText(e.target.value)}
+          required
+          id="outlined-textarea"
+          label="Descrição do ativo"
+          placeholder="Escreva aqui..."
+          multiline
+          // value={form.problemDescription}
+          onChange={(e) => setNomeAtivo(e.target.value)}
         />
+        <TextField
+          required
+          id="outlined-required"
+          label="ID Alpha"
+          placeholder="Escreva aqui..."
+          // value={form.operatorName}
+          onChange={(e) => setTombamento(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Unidade do ativo..."
+          placeholder="Escreva aqui..."
+          // value={form.operatorPhone}
+          onChange={(e) => setUnidade(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Setor do ativo..."
+          placeholder="Escreva aqui..."
+          // value={form.nature}
+          onChange={(e) => setSetor(e.target.value)}
+        ></TextField>
+
+        {/**********************************/}
+
         <Button
           className={classes.btn}
           variant="contained"
